298d2dd7f920e9b1f7364eb72a8c68b1ea1bf275
Glitch (hello-node) <none>
2018-08-22 15:16:32 +0000

    ⛺️🛍 Checkpoint ./server.js:1151468/565 ./public/js/main.js:1151468/36

--- server.js
+++ server.js
@@ -5,18 +5,23 @@ var express = require('express');
 var app = express();
 
 app.get( '/', function( request, response ){
-  response.send( '<h1>Hello client</h1>' );
+  response.send( 'Hello node' );
 });
 
-/*
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+
+// and we can define other routes
+app.get( '/html', function( request, response ){
+  response.send( '<h1>Hello HTML</h1>' );
+});
+
+
+// or we can set express to serve files from a specific folder:
+app.use( express.static( 'public' ) );
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get('/file', function( request, response ) {
+  response.sendFile( __dirname + '/public/index.html' );
 });
-*/
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
