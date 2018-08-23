134a7615e0bc9172e77b79e32ff32533038e1b2d
Glitch (hello-node-server) <none>
2018-08-23 08:31:08 +0000

    🍶🗺 Checkpoint ./server.js:1151468/707

--- server.js
+++ server.js
@@ -1,32 +1,40 @@
 // load express package
-var express = require('express' );
+var express = require( "express" );
 
-// create an express object
+// create an express object that will 
 var app = express();
 
-app.get( '/', function( request, response ){
-  response.send( 'Hello node' );
+// when our server receives a request, it needs to know what it should respond
+// so we need to 
+app.get( "/", function( request, response ){
+  response.send( "Hello node" );
 });
 
 
 // and we can define other routes
-app.get( '/html', function( request, response ){
-  response.send( '<h1>Hello HTML</h1>' );
+app.get( "/html", function( request, response ){
+  response.send( "<h1>Hello HTML</h1>" );
 });
 
 
 // or we can set express to serve files from a specific folder:
-app.use( express.static( 'public' ) );
+app.use( express.static( "public" ) );
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get( '/file', function( request, response ) {
-  response.sendFile( __dirname + '/public/index.html' );
+app.get( "/file", function( request, response ) {
+  response.sendFile( __dirname + "/public/index.html" );
 } );
 
-
-app.get( '/json'
+  // we can also respond to a request with data
+app.get( "/json", function( request, response ) {
+  response.json( {
+    key: "value",
+    randomNumber: Math.random() * 10
+  } )
+} );
+// here we are returning a simple JS object as JSON, but it could connect to a database to look for special data and return it
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
