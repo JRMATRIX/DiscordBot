4d4405af1bac648361a83b8119de4c1c6371325f
Glitch (hello-node-server) <none>
2018-08-28 13:19:26 +0000

    🚂🏵 Checkpoint ./server.js:1151468/494

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 // load express package
 var express = require( "express" );
 
-// create an express object that will 
+// create an express object: a Web server
 var app = express();
 
-// when our server receives a request, it needs to know what it should respond
+// when our server receives a certain request, it needs to know what it should respond
 
-// "/" means we are sending a request for "https://hello-node-server.glitch.me/"
+// when the server gets a request for the root of this domain: "/"
+// ( here: "https://hello-node-server.glitch.me/" )
 app.get( "/", function( request, response ) {
-  response.send( "<h1>Hello Express :)</h1>" );
+  response.send( "<h1>Hello :)</h1>" ); // we tell the server to respond with some HTML
+} );
+
+// when the server gets a request for "https://hello-node-server.glitch.me/about"
+app.get( "/about", function( request, response ) {
+  response.send( "<h1>All about Express</h1>" );
 } );
 
 
-app.get( "/", function( request, response ) {
-  response.send( "<h1>Hello Express :)</h1>" );
-} );
-
-// listen for requests :)
+// finally we set the server to listen for requests :)
 app.listen( process.env.PORT );
 // "process.env.PORT"
