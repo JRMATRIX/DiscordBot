2cc77a8a53ab6daea3d18998dd8e937b471aa2e0
Glitch (hello-node-server) <none>
2018-08-28 13:08:15 +0000

    🍪🚠 Checkpoint ./server.js:1151468/473

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ var express = require( "express" );
 var app = express();
 
 // when our server receives a request, it needs to know what it should respond
+
+// "/" means we are sending a request for "https://hello-node-server.glitch.me/"
+app.get( "/", function( request, response ) {
+  response.send( "<h1>Hello Express :)</h1>" );
+} );
+
+
 app.get( "/", function( request, response ) {
   response.send( "<h1>Hello Express :)</h1>" );
 } );
 
 // listen for requests :)
 app.listen( process.env.PORT );
+// "process.env.PORT"
