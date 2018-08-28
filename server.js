12eef5f27409a58c70258d7a5adb3edb7785913c
Glitch (hello-node-server) <none>
2018-08-28 09:41:05 +0000

    🗾😥 Checkpoint ./package.json:1151468/340 ./server.js:1151468/127

--- server.js
+++ server.js
@@ -5,12 +5,9 @@ var express = require( "express" );
 var app = express();
 
 // when our server receives a request, it needs to know what it should respond
-// so we need to 
 app.get( "/", function( request, response ) {
   response.send( "<h1>Hello Express :)</h1>" );
 } );
 
 // listen for requests :)
-var listener = app.listen( process.env.PORT, function() {
-  console.log( "Your app is listening on port " + listener.address().port );
-} );
+app.listen( process.env.PORT );
