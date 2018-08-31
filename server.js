e6f66b94bf9a5208b6d12458a0bbf1b44105c205
Glitch (hello-node-server) <none>
2018-08-31 09:01:13 +0000

    🌖🐟 Checkpoint ./package.json:1151468/223 ./server.js:1151468/217

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ var express = require( "express" );
 // create an express object: a Web server
 var app = express();
 
-// when our server receives a certain request, it needs to know what it should respond
-
-// when the server gets a request for the root of this domain: "/"
+// When our server receives a certain request, it needs to know what it should respond,
+// so we are defining "routes" the server follows to return the right data.
+// When the server gets a request for the "root" route of this domain: "/"
 // here: "https://hello-node-server.glitch.me/"
 app.get( "/", function( request, response ) {
-  response.send( "<h1>Hello :)</h1>" ); // we tell the server to respond with some HTML
+  response.send( "<h1>Hello :)</h1><p><a href='/about'>About</a></p>" ); // we tell the server to respond with some HTML
 } );
 
 // when the server gets a request for "https://hello-node-server.glitch.me/about"
 app.get( "/about", function( request, response ) {
-  response.send( "<h1>This is my about page</h1>" );
+  response.send( "<h1>This is my about page</h1><a href='/'>Home</a></p>" );
 } );
 
 
