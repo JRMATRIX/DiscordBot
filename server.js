97b728f9418751fb95f388bc905655e5a69c16f2
Glitch (the-real-world) <none>
2019-02-19 22:22:40 +0000

    🛍🗽 Checkpoint ./server.js:3193991/238 ./package.json:3193991/358

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const Discord = require('discord.js');
 const Mixer = require('@mixer/client-node');
+const DB = require('flatfile');
 
 const bot = new Discord.Client();
+
+DB.db('database/mixer.json', ( err, data ) => {
+  if( err ) throw err;
+  
+  data.save( err => {
+    if( err ) throw err; 
+  });
+})
 
 bot.on('message', msg => {  
   
   // Don't read commands from the bot account, look for '!' to read for commands
-  if (msg.author.username != 'TRW Bot' && msg.content.substring(0, 1) == '!' ) {
+  if (msg.author.username != 'TRW Bot' && msg.content.substring(0, 1) == '!' && msg.channel.name === 'bot-configuration' ) {
     
     // msg.channel.send( '*Message received in #' + msg.channel.name + '*' );
     
