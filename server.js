a3955ad2184f44aecbd1e2c434c1bfa7f8b92d85
Glitch (the-real-world) <none>
2019-02-19 21:11:10 +0000

    🎤✌️ Checkpoint ./server.js:3193991/22

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const client = new Discord.Client();
 
 client.on('message', msg => {  
   // Don't read commands from the bot account, look for '!' to read for commands
-  if (msg.author.username != 'TRW Bot' && msg.substring(0, 1) == '!' ) {
+  if (msg.author.username != 'TRW Bot' && msg.content.substring(0, 1) == '!' ) {
     
-    var args = msg.substring(1).split(' ');
+    var args = msg.content.substring(1).split(' ');
     var call = args[0];
     
     if( call == 'trw' ) parse( msg, args );
@@ -14,7 +14,7 @@ client.on('message', msg => {
 })
 
 function parse( msg, args ) {
-  msg.reply( 'got a command
+  msg.reply( 'got a command' );
 }
 
 client.login(process.env.BOT_TOKEN);
