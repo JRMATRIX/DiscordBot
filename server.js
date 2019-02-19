76d5e8c08cf6c62951b43c18c319f00026cc1148
Glitch (the-real-world) <none>
2019-02-19 21:27:30 +0000

    🛥🏂 Checkpoint ./server.js:3193991/705 ./package.json:3193991/296

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const Mixer = require('@mixer/client-node');
 
-client.on('message', msg => {  
+const bot = new Discord.Client();
+
+bot.on('message', msg => {  
+  // msg.channel.send( '*Message received in ' + msg.channel.name + '*' );
+  
   // Don't read commands from the bot account, look for '!' to read for commands
   if (msg.author.username != 'TRW Bot' && msg.content.substring(0, 1) == '!' ) {
     
@@ -14,7 +18,27 @@ client.on('message', msg => {
 })
 
 function parse( msg, args ) {
-  msg.reply( 'got a command' );
+  // msg.channel.send( '*Command received*' );
+  
+  switch( args[1] ) {
+  
+    case 'mixer' :
+      addMixerChannel( msg, args[2], args[3] );
+      break;
+      
+    case 'twitch' :
+      
+      break;
+      
+    case 'youtube' :
+      
+      break;
+      
+  }
 }
 
-client.login(process.env.BOT_TOKEN);
+function addMixerChannel( msg, username, channel ) {
+  msg.channel.send( '*Adding mixer stream ' + username + ' to ' + channel + '*' );
+}
+
+bot.login(process.env.BOT_TOKEN);
