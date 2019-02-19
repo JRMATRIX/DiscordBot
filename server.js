2aa0e02337313b70a7b8c1d6300e0f3f7dc3fdea
Glitch (the-real-world) <none>
2019-02-19 20:38:15 +0000

    📖👣 Checkpoint ./server.js:3193991/218

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('message', ( user, userID, channelID, msg, evt ) => {
-  if (msg.content === 'ping') {
-    msg.reply('pong')
+client.on('message', msg => {  
+  if (msg.author.username != 'TRW Bot' && ) {
+    parse( msg );
   }
 })
+
+function parse( msg ) {
+  
+}
 
 client.login(process.env.BOT_TOKEN);
