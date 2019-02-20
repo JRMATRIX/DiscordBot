360000ee876d272ac2ca8731214f20f9761a9331
Glitch (the-real-world) <none>
2019-02-20 17:56:36 +0000

    🍼☄️ Checkpoint ./server.js:3193991/344

--- server.js
+++ server.js
@@ -209,6 +209,8 @@ function createMixerEmbed( data ) {
     .setFooter( 'The Real World', 'https://cdn.discordapp.com/avatars/547391401000828938/26da8949887ea34cbd3ad3edab407b7c.png?size=256' )
     .setTimestamp( new Date() );
   
+  var announcementChannel = bot.guilds.channels.find( ch => ch.name === data.announcementChannel );
+  console.log( announcementChannel );
   // data.announcementChannel.send( embed );
   
 }
