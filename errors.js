204f53045f7a8a8ecc206bb05a73ffa243ce6247
Glitch (the-real-world) <none>
2019-02-20 11:28:19 +0000

    👲💳 Checkpoint ./errors.js:3193991/247

--- errors.js
+++ errors.js
@@ -1,7 +1,10 @@
 module.exports = {
  
-  mixerChannelExists : function() {
-    var out = "That streamer has already been added to the Mixer Announcement list.\n\n";
-    To update this streamer, you can run the following command:\n**!trw update mixer *<mixerName>* *<announcementChannel>* *<discordUsername>* *<twitterHandle>***'
+  mixerChannelExists : "That streamer has already been added to the Mixer Announcement list.\n\n"
   
+      + "To update this streamer, you can run the following command:\n"
+      + "**!trw update mixer *<mixerName>* *<announcementChannel>* *<discordUsername>* *<twitterHandle>***\n\n"
+  
+      + "To remove this streamer, you can run the following command:\n"
+      + "**!trw remove mixer *<mixerName>***"
 }
